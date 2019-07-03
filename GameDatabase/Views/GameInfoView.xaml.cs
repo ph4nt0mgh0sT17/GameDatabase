37610e89f04260d59a9c55e7eda71e850f0af203
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-04 00:48:45 +0200

    Added GameInfoView to visualize game information. Added DataContext.

---
+++ GameDatabase/Views/GameInfoView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using GameDatabase.Models.ApiModel;
+using GameDatabase.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameDatabase.Views
+{
+    /// <summary>
+    /// Interaction logic for GameInfoView.xaml
+    /// </summary>
+    public partial class GameInfoView : UserControl
+    {
+        public GameInfoView(GameInformationModel game)
+        {
+            InitializeComponent();
+            DataContext = new GameInfoViewModel(game);
+        }
+    }
+}
