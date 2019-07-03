39006c3a363ea06f9a155d2d8d473a2b96680597
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-04 00:48:05 +0200

    Added scroll viewer and getting game asynchronous method.

--- GameDatabase/ViewModels/MainWindowViewModel.cs
+++ GameDatabase/ViewModels/MainWindowViewModel.cs
@@ -5,9 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using GameDatabase.API;
 using GameDatabase.Models.ApiModel;
-
+using GameDatabase.Views;
 
 namespace GameDatabase.ViewModels
 {
@@ -17,17 +18,22 @@ namespace GameDatabase.ViewModels
         private ApiEngine apiEngine;
         private GameSearchResult selectedGame;
         private ObservableCollection<GameSearchResult> gameSearchResults = new ObservableCollection<GameSearchResult>();
+        private GameInfoViewModel gameModel;
+        private ScrollViewer gameInformation;
 
         /// <summary>
         /// Initiliazing ViewModel
         /// </summary>
-        public MainWindowViewModel()
+        public MainWindowViewModel(ScrollViewer gameInformation)
         {
             // Initialize of Api engine
             apiEngine = new ApiEngine();
 
             // Setting visibility of results as hidden
             GameSearchResultsVisibility = Visibility.Hidden;
+
+            // Adding ScrollViewer from View into ViewModel so we can handle it
+            GameInformation = gameInformation;
         }
 
         /// <summary>
@@ -39,6 +45,30 @@ namespace GameDatabase.ViewModels
             GameSearchResults.Clear();
             List<GameSearchResult> gameResults = await apiEngine.GetSearchedGames(queryGame);
             gameResults.ToList().ForEach(GameSearchResults.Add);
+        }
+
+        private async void GetGame(GameSearchResult game)
+        {
+            GameInformationModel gameInfo = await apiEngine.GetGame(game);
+
+            GameInfoView view = new GameInfoView(gameInfo);
+            GameInformation.Content = new GameInfoView(gameInfo);
+        }
+        
+        /// <summary>
+        /// Need this scroll viewer to know where to add GameInformationView
+        /// </summary>
+        public ScrollViewer GameInformation
+        {
+            get
+            {
+                return gameInformation;
+            }
+
+            set
+            {
+                gameInformation = value;
+            }
         }
 
         /// <summary>
@@ -89,7 +119,13 @@ namespace GameDatabase.ViewModels
 
             set
             {
-                selectedGame = value;
+                if (value != null)
+                {
+                    selectedGame = value;
+                    GameSearchResults.Clear();
+                    GameSearchResultsVisibility = Visibility.Hidden;
+                    GetGame(value);
+                }
             }
         }
 
