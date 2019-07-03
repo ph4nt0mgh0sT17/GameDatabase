cc0b98f7c00b997d2ff6f8494b5aa54eb724eaa2
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-03 23:04:53 +0200

    Added some bussiness logic betwen MainWindowViewModel and View for searching games.

--- GameDatabase/ViewModels/MainWindowViewModel.cs
+++ GameDatabase/ViewModels/MainWindowViewModel.cs
@@ -4,54 +4,126 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using GameDatabase.API;
 using GameDatabase.Models.ApiModel;
 
+
 namespace GameDatabase.ViewModels
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : DependencyObject
     {
-        private string query;
+        private string searchQuery;
         private ApiEngine apiEngine;
+        private GameSearchResult selectedGame;
+        private ObservableCollection<GameSearchResult> gameSearchResults = new ObservableCollection<GameSearchResult>();
 
-        private ObservableCollection<GameSearchResult> results = new ObservableCollection<GameSearchResult>();
+        /// <summary>
+        /// Initiliazing ViewModel
+        /// </summary>
         public MainWindowViewModel()
         {
+            // Initialize of Api engine
             apiEngine = new ApiEngine();
+
+            // Setting visibility of results as hidden
+            GameSearchResultsVisibility = Visibility.Hidden;
         }
 
+        /// <summary>
+        /// Gets games by parameter query game
+        /// </summary>
+        /// <param name="queryGame">Text of the game that is searched</param>
         private async void GetGames(string queryGame)
         {
-            Results.Clear();
+            GameSearchResults.Clear();
             List<GameSearchResult> gameResults = await apiEngine.GetSearchedGames(queryGame);
-            gameResults.ToList().ForEach(Results.Add);
+            gameResults.ToList().ForEach(GameSearchResults.Add);
         }
 
-        public ObservableCollection<GameSearchResult> Results
+        /// <summary>
+        /// Game search results
+        /// </summary>
+        public ObservableCollection<GameSearchResult> GameSearchResults
         {
             get
             {
-                return results;
+                return gameSearchResults;
             }
 
             set
             {
-                results = value;
+                gameSearchResults = value;
             }
         }
+        
+        /// <summary>
+        /// Dependency property for visibility of search results so it can be changable during program
+        /// </summary>
+        public static DependencyProperty SearchResultsVisibility =
+            DependencyProperty.Register("GameSearchResultsVisibility", typeof(Visibility), typeof(MainWindowViewModel), new UIPropertyMetadata(Visibility.Hidden));
 
-        public string Query
+        public Visibility GameSearchResultsVisibility
         {
             get
             {
-                return query;
+                return (Visibility)GetValue(SearchResultsVisibility);
             }
 
             set
             {
-                query = value;
-                GetGames(value);
+                SetValue(SearchResultsVisibility, value);
             }
         }
+
+        
+        /// <summary>
+        /// Selected game in SearchResults
+        /// </summary>
+        public GameSearchResult SelectedGame
+        {
+            get
+            {
+                return selectedGame;
+            }
+
+            set
+            {
+                selectedGame = value;
+            }
+        }
+
+        /// <summary>
+        /// Text in textbox -> searching query
+        /// </summary>
+        public string SearchQuery
+        {
+            get
+            {
+                return searchQuery;
+            }
+
+            set
+            {
+                searchQuery = value;
+
+                // If value has something then search results
+                if (value != "")
+                {
+                    GameSearchResultsVisibility = Visibility.Visible;
+                    GetGames(value);
+                }
+
+                // If value has empty string then set visibility as hidden
+                // And clear all results
+                else
+                {
+                    GameSearchResults.Clear();
+                    GameSearchResultsVisibility = Visibility.Hidden;
+                }
+            }
+        }
+
+
     }
 }
