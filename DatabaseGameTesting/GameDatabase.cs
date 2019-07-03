f34768e1a30641e9f4133d8f2d29ae84a2dfb9f0
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-04 01:09:41 +0200

    Added Unit testing project and first method.

---
+++ DatabaseGameTesting/GameDatabase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameDatabase.API;
+using GameDatabase.Models.ApiModel;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DatabaseGameTesting
+{
+    [TestClass]
+    public class GameDatabase
+    {
+        /// <summary>
+        /// Test method for getting game if API_KEY works as it should...
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task GettingGame()
+        {
+            string searchedGame = "The Witcher 3";
+            ApiEngine apiEngine = new ApiEngine();
+
+            List<GameSearchResult> game = await apiEngine.GetSearchedGames(searchedGame);
+
+            Assert.IsFalse(game.Count == 0);
+        }
+    }
+}
---
+++ DatabaseGameTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DatabaseGameTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DatabaseGameTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("888fea2f-b6ec-4536-b906-188af8e64cc8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
