bdea106e12b139b13b6483456675166cda69d153
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-04 00:35:13 +0200

    Addeed new API query to ApiEngine

--- GameDatabase/API/ApiEngine.cs
+++ GameDatabase/API/ApiEngine.cs
@@ -53,8 +53,34 @@ namespace GameDatabase.API
             List<GameSearchResult> gameResults = JsonConvert.DeserializeObject<List<GameSearchResult>>(jsonResponse);
 
             return gameResults;
+        }
 
+        /// <summary>
+        /// Added new API query that will get all the information about current game
+        /// </summary>
+        public async Task<GameInformationModel> GetGame(GameSearchResult searchedGame)
+        {
+            // Request message that will be sent to API endpoint
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, GAME_URL);
 
+            string jsonPost = $"fields *; where name = \"{searchedGame.Name}\";";
+
+            // Adding Accept header to make sure that output will be json
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Headers.Add("user-key", API_KEY); // Add user-key to header
+
+            // Added json body to request 
+            request.Content = new StringContent(jsonPost, Encoding.UTF8, "application/json");
+
+            // Request message is asynchronously posted
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+
+            // Read json body of the response
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+
+            List<GameInformationModel> games = JsonConvert.DeserializeObject<List<GameInformationModel>>(jsonResponse);
+
+            return games[0];
         }
     }
 }
