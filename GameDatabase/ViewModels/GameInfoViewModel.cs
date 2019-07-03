08edcdd18c7ef9a42a3a4bce73492f2a58a6d90d
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-04 00:36:59 +0200

    Created new ViewModel for GameInformationView which handles obtaining game from MainWindowViewModel.

---
+++ GameDatabase/ViewModels/GameInfoViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using GameDatabase.Models.ApiModel;
+
+namespace GameDatabase.ViewModels
+{
+    public class GameInfoViewModel : DependencyObject
+    {
+        private GameInformationModel game;
+
+        /// <summary>
+        /// Will set all information about game
+        /// </summary>
+        public GameInfoViewModel(GameInformationModel game)
+        {
+            Game = game;
+        }
+
+        public static DependencyProperty GameDependency =
+            DependencyProperty.Register("Game", 
+                typeof(GameInformationModel), 
+                typeof(GameInfoViewModel), 
+                new PropertyMetadata(
+                    new GameInformationModel()));
+        public GameInformationModel Game
+        {
+            get
+            {
+                return (GameInformationModel)GetValue(GameDependency);
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    SetValue(GameDependency, value);
+                }
+            }
+        }
+    }
+}
