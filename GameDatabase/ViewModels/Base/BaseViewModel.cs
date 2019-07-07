3da5293d8d699d0ed20dec4c1a22507823886d0e
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-07 23:57:48 +0200

    Created a BaseViewModel for all view models for INotifyPropertyChanged handler

---
+++ GameDatabase/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDatabase
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        #region Public events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
