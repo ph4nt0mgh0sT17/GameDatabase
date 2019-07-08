1432bf1a2f44b9b51a5dab0c7a39ebb32510e7a0
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-08 08:44:08 +0200

    Added new Window and AsyncRelayCommand, so we can use Async functions now for our application which will be very good.

---
+++ GameDatabase/ViewModels/Base/AsyncRelayCommand.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Dispatcher;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Input;
+
+namespace GameDatabase
+{
+    public class AsyncRelayCommand : ICommand
+    {
+        #region Private members
+
+        private bool mIsExecuting;
+        private readonly Func<Task> mExecute;
+        private readonly Func<bool> mCanExecute;
+        private readonly IErrorHandler mErrorHandler;
+
+        #endregion
+
+        #region Public events
+
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// This constructor creates command that will be surely executed 
+        /// </summary>
+        /// <param name="methodToBeExecuted">Method that will be executed</param>
+        public AsyncRelayCommand(Func<Task> methodToBeExecuted)
+        {
+            this.mExecute = methodToBeExecuted;
+            mCanExecute = null;
+            mErrorHandler = null;
+        }
+
+
+        /// <summary>
+        /// Creates command with <see cref="mExecute" /> that will be executed if <see cref="mCanExecute"/> will be true/>
+        /// </summary>
+        /// <param name="methodToBeExecuted">Method that will be executed</param>
+        /// <param name="canBeMethodToBeExecuted">Boolean parameter that determines whether the method will be executed or not</param>
+        public AsyncRelayCommand(Func<Task> methodToBeExecuted, Func<bool> canBeMethodToBeExecuted)
+        {
+            this.mExecute = methodToBeExecuted;
+            this.mCanExecute = canBeMethodToBeExecuted;
+            mErrorHandler = null;
+        }
+
+        #endregion
+
+
+
+        #region Command methods
+
+        public bool CanExecute()
+        {
+            return !mIsExecuting && (mCanExecute?.Invoke() ?? true);
+        }
+
+        public async Task ExecuteAsync()
+        {
+            if (CanExecute())
+            {
+                try
+                {
+                    mIsExecuting = true;
+                    RaiseCanExecuteChanged();
+                    await mExecute();
+                }
+
+                finally
+                {
+                    mIsExecuting = false;
+                }
+            }
+
+            RaiseCanExecuteChanged();
+        }
+
+        /// <summary>
+        /// Fires everytime that '<see cref="mCanExecute"/>' changes...
+        /// </summary>
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// Determines if command can be executed
+        /// </summary>
+        bool ICommand.CanExecute(object parameter)
+        {
+            return CanExecute();
+        }
+
+
+        /// <summary>
+        /// Executes the command if command is not null
+        /// </summary>
+        void ICommand.Execute(object parameter)
+        {
+            ExecuteAsync();
+        }
+
+        #endregion
+    }
+}
---
+++ GameDatabase/ViewModels/GameInformationWindowViewModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDatabase
+{
+    public class GameInformationWindowViewModel : BaseViewModel
+    {
+        #region Private members
+
+        string mVersion;
+
+        #endregion
+
+        private int counter;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public GameInformationWindowViewModel()
+        {
+            Version = "Game Database 1.00";
+            GenerateVersionCommand = new AsyncRelayCommand(GenerateVersion);
+            counter = 0;
+        }
+        
+        /// <summary>
+        /// Automatically generates asynchronous method which generates version of the application for test purposes
+        /// </summary>
+        /// <returns></returns>
+        private async Task GenerateVersion()
+        {
+            counter = 1;
+            // Clear version
+            Version = "Game Database v1.";
+
+            // Instantiate generator of automatic generation...
+            Random generator = new Random();
+
+            // Loop for generating some chars from ASCII table for test if binding works
+            for (int i = 0; i < 4; i++)
+            {
+                int generatedNumber = generator.Next(0, 10);
+                Version += generatedNumber.ToString();
+                await Task.Delay(100);
+            }
+            counter = 0;
+        }
+
+        private bool CanExecute()
+        {
+            return counter == 0;
+        }
+
+        #region Properties
+
+        /// <summary>
+        /// Version of the application
+        /// </summary>
+        public string Version
+        {
+            get
+            {
+                return mVersion;
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    mVersion = value;
+                    RaisePropertyChanged(nameof(Version));
+                }
+            }
+        }
+
+        public AsyncRelayCommand GenerateVersionCommand
+        {
+            get; set;
+        }
+
+        #endregion
+    }
+}
