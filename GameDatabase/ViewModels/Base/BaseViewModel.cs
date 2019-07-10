3c381475c870c2ca7a406da93e4b03ebd0c5669b
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-10 17:54:49 +0200

    Added some properties to BaseViewModel.

--- GameDatabase/ViewModels/Base/BaseViewModel.cs
+++ GameDatabase/ViewModels/Base/BaseViewModel.cs
@@ -9,9 +9,87 @@ namespace GameDatabase
 {
     public class BaseViewModel : INotifyPropertyChanged
     {
+        #region Private fields
+
+        // Resolution of the window -> every view model will have these variables
+        private int mWidth;
+        private int mHeight;
+        private int mMinWidth;
+        private int mMinHeight;
+
+        #endregion
+
         #region Public events
 
         public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Public properties
+
+        /// <summary>
+        /// The width of the window
+        /// </summary>
+        public int Width
+        {
+            get
+            {
+                return mWidth;
+            }
+
+            set
+            {
+                mWidth = value;
+            }
+        }
+        
+        /// <summary>
+        /// The height of the window
+        /// </summary>
+        public int Height
+        {
+            get
+            {
+                return mHeight;
+            }
+
+            set
+            {
+                mHeight = value;
+            }
+        }
+
+        /// <summary>
+        /// Min width of the window
+        /// </summary>
+        public int MinWidth
+        {
+            get
+            {
+                return mMinWidth;
+            }
+
+            set
+            {
+                mMinWidth = value;
+            }
+        }
+
+        /// <summary>
+        /// Minimal height of the window
+        /// </summary>
+        public int MinHeight
+        {
+            get
+            {
+                return mMinHeight;
+            }
+
+            set
+            {
+                mMinHeight = value;
+            }
+        }
 
         #endregion
 
