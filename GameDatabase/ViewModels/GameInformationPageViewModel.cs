4e319a3d30777c9311a85de8f7db9647a882e864
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-10 20:58:27 +0200

    Added Page InformationContent into GameInformationPageViewModel for displaying different pages

--- GameDatabase/ViewModels/GameInformationPageViewModel.cs
+++ GameDatabase/ViewModels/GameInformationPageViewModel.cs
@@ -3,20 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace GameDatabase
 {
-    public class GameInformationPageViewModel
+    public class GameInformationPageViewModel : BaseViewModel
     {
         #region Private fields
 
         private GameInformationModel mGameModel;
+        private Page mContent;
 
         #endregion
 
         public GameInformationPageViewModel(GameInformationModel model)
         {
             GameModel = model;
+            BasicInformation = new RelayCommand(ShowBasicInfo);
+        }
+
+        private void ShowBasicInfo()
+        {
+           InformationContent =  new GameBasicInformationPage(mGameModel);
         }
 
         public GameInformationModel GameModel
@@ -32,6 +40,25 @@ namespace GameDatabase
                 {
                     mGameModel = value;
                 }
+            }
+        }
+
+        public RelayCommand BasicInformation
+        {
+            get; set;
+        }
+
+        public Page InformationContent
+        {
+            get
+            {
+                return mContent;
+            }
+
+            set
+            {
+                mContent = value;
+                RaisePropertyChanged(nameof(InformationContent));
             }
         }
     }
