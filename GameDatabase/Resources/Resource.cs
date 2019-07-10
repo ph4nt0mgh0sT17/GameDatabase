58ad67308dd1320c379cf5d6ce14a8d50f1710dd
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-10 18:14:55 +0200

    Added text resources.

---
+++ GameDatabase/Resources/Resource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDatabase
+{
+    public static class Resource
+    {
+        public static string Title { get; } = "Game database application";
+
+        public static string Version { get; } = "Game database application version 1.0";
+    }
+}
