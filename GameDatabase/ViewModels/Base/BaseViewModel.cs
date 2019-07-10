610a38244682c2d93a47f448fb3fb2f5d9f9216e
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-10 18:45:36 +0200

    Added margins of the window to a BaseViewModel for centering a window, because it doesn't cetner using WindowStartupLocation because Width and Height are not known in advance.

--- GameDatabase/ViewModels/Base/BaseViewModel.cs
+++ GameDatabase/ViewModels/Base/BaseViewModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace GameDatabase
 {
@@ -11,11 +12,23 @@ namespace GameDatabase
     {
         #region Private fields
 
-        // Resolution of the window -> every view model will have these variables
+        #region Resolution of the window
+
         private int mWidth;
         private int mHeight;
         private int mMinWidth;
         private int mMinHeight;
+
+        #endregion
+
+        #region Margin of the window
+
+        private int mLeftWindow;
+        private int mTopWindow;
+
+        #endregion
+
+        private string mTitleApplication;
 
         #endregion
 
@@ -25,7 +38,39 @@ namespace GameDatabase
 
         #endregion
 
+        /// <summary>
+        /// Constructor sets title as default name of the application
+        /// </summary>
+        public BaseViewModel()
+        {
+            // Gets text from Resource folder
+            TitleApplication = Resource.Title;
+
+            Width = 1024;
+            Height = 768;
+
+            CenterScreen();
+        }
+
+        #region Private helper methods
+
+        private void CenterScreen()
+        {
+            int screenWidth = (int)SystemParameters.PrimaryScreenWidth;
+            int screenHeight = (int)SystemParameters.PrimaryScreenHeight;
+
+            int windowWidth = this.Width;
+            int windowHeight = this.Height;
+
+            this.LeftWindow = (screenWidth / 2) - (windowWidth / 2);
+            this.TopWindow = (screenHeight / 2) - (windowHeight / 2);
+        }
+
+        #endregion
+
         #region Public properties
+
+        #region Resolution of the window
 
         /// <summary>
         /// The width of the window
@@ -40,6 +85,7 @@ namespace GameDatabase
             set
             {
                 mWidth = value;
+                RaisePropertyChanged(nameof(Width));
             }
         }
         
@@ -56,6 +102,7 @@ namespace GameDatabase
             set
             {
                 mHeight = value;
+                RaisePropertyChanged(nameof(Height));
             }
         }
 
@@ -88,6 +135,54 @@ namespace GameDatabase
             set
             {
                 mMinHeight = value;
+            }
+        }
+
+        #endregion
+
+        #region Margin of the window
+
+        public int LeftWindow
+        {
+            get
+            {
+                return mLeftWindow;
+            }
+
+            set
+            {
+                mLeftWindow = value;
+            }
+        }
+
+        public int TopWindow
+        {
+            get
+            {
+                return mTopWindow;
+            }
+
+            set
+            {
+                mTopWindow = value;
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// A title of the application
+        /// </summary>
+        public string TitleApplication
+        {
+            get
+            {
+                return mTitleApplication;
+            }
+
+            private set
+            {
+                mTitleApplication = value;
             }
         }
 
