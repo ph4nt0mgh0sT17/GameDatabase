cf990d8ce92f9ea09b68eca78c778c690f8dcd43
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-10 20:38:26 +0200

    Added GameInformationPage to the application. Here will be everything about searched game.

---
+++ GameDatabase/ViewModels/GameInformationPageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDatabase
+{
+    public class GameInformationPageViewModel
+    {
+        #region Private fields
+
+        private GameInformationModel mGameModel;
+
+        #endregion
+
+        public GameInformationPageViewModel(GameInformationModel model)
+        {
+            GameModel = model;
+        }
+
+        public GameInformationModel GameModel
+        {
+            get
+            {
+                return mGameModel;
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    mGameModel = value;
+                }
+            }
+        }
+    }
+}
