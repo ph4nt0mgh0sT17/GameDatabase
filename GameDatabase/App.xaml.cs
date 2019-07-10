07d0714b12ddedfb5b17ccb77581293df9caee31
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-10 18:16:16 +0200

    Added OnStartup method in App.xaml.cs

--- GameDatabase/App.xaml.cs
+++ GameDatabase/App.xaml.cs
@@ -13,5 +13,14 @@ namespace GameDatabase
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// This method is executed when the application gets started... 
+        /// </summary>
+        /// <param name="e">Some event arguements</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindowView mainWindow = new MainWindowView();
+            mainWindow.Show();
+        }
     }
 }
