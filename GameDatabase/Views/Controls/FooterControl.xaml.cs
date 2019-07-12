1a9e326a3f6fe2a8c8159e0bbb4f68078b126c93
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-12 22:57:04 +0200

    Changed using directives added some designerproperties tool and changed ApplicationAuthor getter.

--- GameDatabase/Views/Controls/FooterControl.xaml.cs
+++ GameDatabase/Views/Controls/FooterControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,32 +13,76 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using GameDatabaseResources;
+using GameDatabaseResources.Footer;
 
 namespace GameDatabase
 {
     /// <summary>
-    /// Interaction logic for FooterControl.xaml
+    /// Footer user control 
     /// </summary>
     public partial class FooterControl : UserControl
     {
+        /// <summary>
+        /// Constructor which sets FooterWidth if in designer
+        /// </summary>
         public FooterControl()
         {
             InitializeComponent();
+
+            if (DesignerProperties.GetIsInDesignMode(this))
+            {
+                FooterWidth = 800;
+            }
+
             DataContext = this;
         }
 
-        public string FooterContent { get; } = Texts.FooterVersion;
+        #region Properties
 
+        /// <summary>
+        /// Footer content -> Footer version of the application
+        /// </summary>
+        public string FooterContent
+        {
+            get
+            {
+                return Texts.FooterVersion;
+            }
+        }
+
+        /// <summary>
+        /// Gets the author of the application
+        /// </summary>
+        public string ApplicationAuthor
+        {
+            get
+            {
+                return $"{Texts.AuthorString}: {Texts.Author}";
+            }
+        }
+
+        /// <summary>
+        /// Gets the width of a footer
+        /// </summary>
         public int FooterWidth
         {
-            get { return (int)GetValue(FooterWidthProperty); }
+            get
+            {
+                return (int)GetValue(FooterWidthProperty);
+            }
+
             set { SetValue(FooterWidthProperty, value); }
         }
+
+        #endregion
+
+        #region Dependency properties
 
         // Using a DependencyProperty as the backing store for FooterWidth.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty FooterWidthProperty =
             DependencyProperty.Register("FooterWidth", typeof(int), typeof(FooterControl), new PropertyMetadata(0));
+
+        #endregion
 
 
     }
