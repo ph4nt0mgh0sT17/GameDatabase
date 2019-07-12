7239ca175018feddf7ee2c059ca921278c91ee40
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-12 21:57:10 +0200

    Created Texts.Designer.cs which gets all names from Texts.resx, which is unique xaml file where all texts are stored.

---
+++ GameDatabaseResources/Texts.Designer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Globalization;
+using System.Resources;
+
+namespace GameDatabaseResources
+{
+    public class Texts
+    {
+
+        private static ResourceManager mResourceManager;
+        private static CultureInfo mCultureInfo;
+
+        public Texts()
+        {
+        }
+
+        private static ResourceManager TextManager
+        {
+            get
+            {
+                if (object.Equals(mResourceManager, null))
+                {
+                    mResourceManager = new ResourceManager("GameDatabaseResources.Texts", typeof(Texts).Assembly);
+                }
+
+                return mResourceManager;
+            }
+        }
+
+        private static CultureInfo CultureInformation
+        {
+            get
+            {
+                return mCultureInfo;
+            }
+
+            set
+            {
+                mCultureInfo = value;
+            }
+        }
+
+        public static string ApplicationName
+        {
+            get
+            {
+                return TextManager.GetString(nameof(ApplicationName), CultureInformation);
+            }
+        }
+
+        public static string FooterVersion
+        {
+            get
+            {
+                return TextManager.GetString(nameof(FooterVersion), CultureInformation);
+            }
+        }
+
+        public static string Author
+        {
+            get
+            {
+                return TextManager.GetString(nameof(Author), CultureInformation);
+            }
+        }
+    }
+}
