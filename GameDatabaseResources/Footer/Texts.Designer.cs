d2bb3cd2986f3b992197dec95a8713bb7c162d8d
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-12 22:55:49 +0200

    Totally remade  GameDatabaseResources to folders so I know where these texts depends.

---
+++ GameDatabaseResources/Footer/Texts.Designer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Globalization;
+using System.Resources;
+
+namespace GameDatabaseResources.Footer
+{
+    public class Texts
+    {
+
+        private static ResourceManager mResourceManager;
+        private static CultureInfo mCultureInfo;
+
+        public Texts()
+        {
+        }
+
+        private static ResourceManager TextManager
+        {
+            get
+            {
+                if (object.Equals(mResourceManager, null))
+                {
+                    mResourceManager = new ResourceManager("GameDatabaseResources.Footer.Texts", typeof(Texts).Assembly);
+                }
+
+                return mResourceManager;
+            }
+        }
+
+        private static CultureInfo CultureInformation
+        {
+            get
+            {
+                return mCultureInfo;
+            }
+
+            set
+            {
+                mCultureInfo = value;
+            }
+        }
+
+        /// <summary>
+        /// Version of the game displayed in the footer
+        /// </summary>
+        public static string FooterVersion
+        {
+            get
+            {
+                return TextManager.GetString(nameof(FooterVersion), CultureInformation);
+            }
+        }
+
+        /// <summary>
+        /// The name of the author of the application
+        /// </summary>
+        public static string Author
+        {
+            get
+            {
+                return TextManager.GetString(nameof(Author), CultureInformation);
+            }
+        }
+
+        /// <summary>
+        /// A string for displaying name of author
+        /// </summary>
+        public static string AuthorString
+        {
+            get
+            {
+                return TextManager.GetString(nameof(AuthorString), CultureInformation);
+            }
+        }
+    }
+}
