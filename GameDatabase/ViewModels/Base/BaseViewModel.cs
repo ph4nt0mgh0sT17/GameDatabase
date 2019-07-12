5c58d348146de7cb55511685d9306ed4d8274195
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-12 22:57:32 +0200

    Changed namespace to proper folder.

--- GameDatabase/ViewModels/Base/BaseViewModel.cs
+++ GameDatabase/ViewModels/Base/BaseViewModel.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using GameDatabaseResources;
+using GameDatabaseResources.Application;
 
 namespace GameDatabase
 {
