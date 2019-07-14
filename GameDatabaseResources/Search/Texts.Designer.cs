ccf2aeb557c535dafade4b34bb6cf4edd30657b7
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-14 11:55:34 +0200

    And added new field for texts which is Search window.

---
+++ GameDatabaseResources/Search/Texts.Designer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using System.Resources;
+
+namespace GameDatabaseResources.Search
+{
+    public class Texts
+    {
+
+        private static ResourceManager mResourceManager;
+        private static CultureInfo mCultureInfo;
+
+        public Texts()
+        {
+        }
+
+        private static ResourceManager TextManager
+        {
+            get
+            {
+                if (object.Equals(mResourceManager, null))
+                {
+                    mResourceManager = new ResourceManager("GameDatabaseResources.Search.Texts", typeof(Texts).Assembly);
+                }
+
+                return mResourceManager;
+            }
+        }
+
+        private static CultureInfo CultureInformation
+        {
+            get
+            {
+                return mCultureInfo;
+            }
+
+            set
+            {
+                mCultureInfo = value;
+            }
+        }
+
+        public static string SearchText
+        {
+            get
+            {
+                return TextManager.GetString(nameof(SearchText), CultureInformation);
+            }
+        }
+    }
+}
