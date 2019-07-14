a894fd235815c356d760c038804f905e7f54ee09
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-14 11:54:49 +0200

    Added to App.xaml.cs loading language to the application. There will be a new class for setting these things and so on, but they will be invoked in this App.xaml.cs class.

--- GameDatabase/App.xaml.cs
+++ GameDatabase/App.xaml.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -19,6 +23,38 @@ namespace GameDatabase
         /// <param name="e">Some event arguements</param>
         protected override void OnStartup(StartupEventArgs e)
         {
+            #region Reading Settings file
+
+            string applicationPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string settingPath = Path.Combine(applicationPath, "Setting.txt");
+            // Checking if there is Settings file
+            if (File.Exists($"{settingPath}"))
+            {
+                // Reading from settings file -> This is only testing purpose but there will be more than 1 line in the setting file
+                using (StreamReader reader = new StreamReader($"{ applicationPath }\\Setting.txt"))
+                {
+                    // Reading a line from Setting.txt file
+                    string line = reader.ReadLine();
+
+                    // Getting the culture information -> Language, which is I believe cs-cz
+                    string cultureInformation = line.Split('=')[1];
+
+                    Setting.Language = cultureInformation;
+                }
+            }
+
+            // If there is no language set language as default
+            else
+            {
+                Setting.Language = "en-US";
+            }
+
+            #endregion
+            // Set czech language as default
+            CultureInfo currentCulture = new CultureInfo(Setting.Language);
+            Thread.CurrentThread.CurrentCulture = currentCulture;
+            Thread.CurrentThread.CurrentUICulture = currentCulture;
+
             MainWindowView mainWindow = new MainWindowView();
             mainWindow.Show();
         }
