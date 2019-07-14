0c99d0b3214cce5e69010f48ae8fdea215dcf026
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-14 15:37:14 +0200

    Added style triggers, next time it will be divided to style file.

---
+++ GameDatabaseResources/Menu/Texts.Designer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using System.Resources;
+
+namespace GameDatabaseResources.Menu
+{
+    public class Texts
+    {
+
+        private static ResourceManager mResourceManager;
+        private static CultureInfo mCultureInfo;
+
+        public Texts()
+        {
+        }
+
+        private static ResourceManager TextManager
+        {
+            get
+            {
+                if (object.Equals(mResourceManager, null))
+                {
+                    mResourceManager = new ResourceManager("GameDatabaseResources.Menu.Texts", typeof(Texts).Assembly);
+                }
+
+                return mResourceManager;
+            }
+        }
+
+        private static CultureInfo CultureInformation
+        {
+            get
+            {
+                return mCultureInfo;
+            }
+
+            set
+            {
+                mCultureInfo = value;
+            }
+        }
+
+        public static string ApplicationName
+        {
+            get
+            {
+                return TextManager.GetString(nameof(ApplicationName), CultureInformation);
+            }
+        }
+
+        public static string SettingsMenu
+        {
+            get
+            {
+                return TextManager.GetString(nameof(SettingsMenu), CultureInformation);
+            }
+        }
+    }
+}
