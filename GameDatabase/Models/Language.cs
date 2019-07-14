a216f32665e2939d767e05e981fa35e4431d1414
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-14 13:20:00 +0200

    Created own class for language.

---
+++ GameDatabase/Models/Language.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDatabase
+{
+    public class Language
+    {
+        #region Private fields
+
+        private string mTitle;
+        private string mName;
+
+        #endregion
+
+        public Language(string mTitle, string mName)
+        {
+            this.mTitle = mTitle;
+            this.mName = mName;
+        }
+
+        public string Title
+        {
+            get
+            {
+                return mTitle;
+            }
+
+            set
+            {
+                mTitle = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return mName;
+            }
+
+            set
+            {
+                mName = value;
+            }
+        }
+    }
+}
