e7a32211d1e5ed84bfd632b5b8c81814d46fbe7a
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-07-14 11:52:33 +0200

    Added new Setting file, it will be in Setting folder so everyone know what will be here.

---
+++ GameDatabase/Setting/Setting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDatabase
+{
+    /// <summary>
+    /// Static class Setting for setting some options, language, resolution (maybe) and so on
+    /// </summary>
+    public static class Setting
+    {
+        public static string Language
+        {
+            get; set;
+        }
+    }
+}
